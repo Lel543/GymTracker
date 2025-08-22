7926319094cbce8781c831d7f8ee05a6ffbabaa9
Lel543 <haidererpaul@gmail.com>
2025-08-22 21:18:34 +0200

    unneccessary bs remoeved

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-row>
+    <v-col cols="12">
+      <Dashboard/>
+    </v-col>
+  </v-row>
+  
 </template>
 
